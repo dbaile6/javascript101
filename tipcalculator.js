b6da538b40822c53afebe75b3d5fb0a098e7ae4c
Dylan Bailey <dylanba1@gmail.com>
2018-02-19 14:15:46 -0500

    Add files via upload

---
+++ tipcalculator.js
@@ -0,0 +1,17 @@
+var promptTotal = prompt('What was the total bill amount?');
+var promptService = prompt('What was the level of service? Select GOOD, FAIR, or BAD');
+var numberOfCustomers = prompt('How many people to split the bill');
+
+if(promptService = 'Good')
+var tip = console.log(promptTotal * .20)
+if(promptService = 'Fair')
+var tip = console.log(promptTotal * .15)
+if(promptService = 'Bad')
+var tip = console.log(PromptTotal * .10)
+
+var billTotal = tip + PromptTotal
+var individualTotal = billTotal / numberOfCustomers
+
+console.log('Your tip is ' + tip)
+console.log('The bill total is ' + billtotal)
+console.log('Each person pays ' + individualTotal)
