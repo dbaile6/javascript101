e394837ce6e80459bb92d988fba2351ca8efdcaa
Dylan Bailey <dylanba1@gmail.com>
2018-07-12 00:51:51 -0500

    Finish chapter 1

--- ydkjs/statements.js
+++ ydkjs/statements.js
@@ -1,3 +1,19 @@
 let a = 21
 let b = a * 2
 console.log(b)
+
+const accessoryprice = 331.25;
+
+let bank_balance = 302.13;
+let amount = 99.99;
+
+amount = amount * 2
+
+if (amount < bank_balance) {
+    console.log("I'll take the accesorry!")
+    amount = amount + ACCESSORY_PRICE;
+}
+
+else {
+    console.log("No thanks!")
+}
